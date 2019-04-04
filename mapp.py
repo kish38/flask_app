078a2f80ce69fcbf31b409abeddeb82090cafdfe
Netala Kishore Kumar <softwaredeveloper@Netalas-MacBook-Air.local>
2019-04-04 18:46:48 +0530

    setup with bootstrap & test data

--- mapp.py
+++ mapp.py
@@ -7,6 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+articles_data = [
+            {'id': 1,
+             'title': 'First Post',
+             'author': 'Kishore',
+             'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores culpa cum deleniti dolore fugiat, in iure laborum nesciunt nostrum quia quod voluptatibus. Aut eos fuga maxime, molestiae odio vel!'
+             },
+             {'id': 2,
+             'title': 'Another Post',
+             'author': 'Kishore',
+             'description': 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi asperiores culpa cum deleniti dolore fugiat, in iure laborum nesciunt nostrum quia quod voluptatibus. Aut eos fuga maxime, molestiae odio vel!'
+             },
+            ]
+
+@app.route('/articles/')
+def articles():
+    return render_template('articles.html', articles=articles_data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
