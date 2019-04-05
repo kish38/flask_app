9ebb7036c05fe496ac7e0d8cc43480ff321271db
kishore <kishorekumarnetala@gmail.com>
2019-04-05 12:45:02 +0530

    user login and login decorator

--- mapp.py
+++ mapp.py
@@ -4,6 +4,7 @@ from flask_mysqldb import MySQL
 from passlib.hash import sha256_crypt
 from mforms import RegisterForm
 from flask import request
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -59,6 +60,57 @@ def register():
 
         return redirect(url_for('index'))
     return render_template('registration.html', form=form)
+
+
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        pwd_raw = request.form['password']
+
+        cur = mysql.connection.cursor()
+
+        results = cur.execute("SELECT * FROM users WHERE username=%s", [username])
+        if results > 0:
+            data = cur.fetchone()
+            pwd = data['password']
+            if sha256_crypt.verify(pwd_raw, pwd):
+                session['login'] = True
+                session['username'] = username
+                flash('You are logged in', 'success')
+
+                return redirect(url_for('articles'))
+            else:
+                flash("Password mismatch", "danger")
+        else:
+            flash("No User Exists", "info")
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("Logged out", 'success')
+    return redirect(url_for('index'))
+
+
+# Login Decorator
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'login' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('You have to login to access', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/profile')
+@is_logged_in
+def profile():
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
